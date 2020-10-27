cbb8f19681b310245f184ac7393fdecf50c0bc1c
Mathieu Lecarme <mlecarme@bearstech.com>
2020-10-27 14:22:49 +0100

    UDP ping.

--- server/server.go
+++ server/server.go
@@ -22,6 +22,29 @@ type Server struct {
 }
 
 func (s *Server) ListenAndServe(address string) error {
+	go func() {
+		a, err := net.ResolveUDPAddr("udp", address)
+		if err != nil {
+			panic(err)
+		}
+		listener, err := net.ListenUDP("udp", a)
+		if err != nil {
+			panic(err)
+		}
+		defer listener.Close()
+		buf := make([]byte, 1024)
+		for {
+			_, addr, err := listener.ReadFromUDP(buf)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			re, _ := net.DialUDP("udp", nil, addr)
+			defer re.Close()
+			re.Write(buf)
+			fmt.Println("Pong")
+		}
+	}()
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
 		return err
