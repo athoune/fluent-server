8a8a72d65bd058f24262925a50497e458a464487
Mathieu Lecarme <mlecarme@bearstech.com>
2020-10-28 23:13:35 +0100

    package message

---
+++ message/message.go
@@ -0,0 +1,181 @@
+package message
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/vmihailenco/msgpack/v5"
+	"github.com/vmihailenco/msgpack/v5/msgpcode"
+)
+
+type Event struct {
+	tag    string
+	ts     time.Time
+	record map[string]interface{}
+}
+
+type HandlerFunc func(tag string, time time.Time, record map[string]interface{}) error
+
+type FluentReader struct {
+	eventHandler HandlerFunc
+}
+
+func New(eventHandler HandlerFunc) *FluentReader {
+	return &FluentReader{
+		eventHandler: eventHandler,
+	}
+}
+
+func (f *FluentReader) Read(reader io.Reader) error {
+	decoder := msgpack.NewDecoder(reader)
+	for {
+		err := f.decodeMessage(decoder)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func (f *FluentReader) decodeMessage(decoder *msgpack.Decoder) error {
+	code, err := decoder.PeekCode()
+	if err != nil {
+		return err
+	}
+	if code == msgpcode.Nil {
+		fmt.Println("Hearthbeat")
+		return nil
+	}
+	if !msgpcode.IsFixedArray(code) {
+		return errors.New("Not an array")
+	}
+	l, err := decoder.DecodeArrayLen()
+	if err != nil {
+		return err
+	}
+	if l == 0 {
+		return errors.New("Empty array")
+	}
+	if l > 10 {
+		return errors.New("Flood")
+	}
+	tag, err := decoder.DecodeString()
+	if err != nil {
+		return err
+	}
+	switch tag {
+	case "HELO":
+		return f.doHelo()
+	case "PING":
+		return f.doPing()
+	case "PONG":
+		return f.doPong()
+	default:
+		return f.decodeEvent(tag, decoder, l)
+	}
+}
+
+func (f *FluentReader) decodeEvent(tag string, decoder *msgpack.Decoder, l int) error {
+	if l < 2 {
+		return errors.New("Too short")
+	}
+	firstCode, err := decoder.PeekCode()
+	if err != nil {
+		return err
+	}
+	switch {
+	case msgpcode.IsFixedArray(firstCode):
+		size, err := decoder.DecodeArrayLen()
+		if err != nil {
+			return err
+		}
+		events := make([]Event, size)
+		for i := 0; i < size; i++ {
+			c, err := decoder.PeekCode()
+			if err != nil {
+				return err
+			}
+			if !msgpcode.IsFixedArray(c) {
+				return fmt.Errorf("Elem %d is not an array : %v", i, c)
+			}
+			l, err := decoder.DecodeArrayLen()
+			if err != nil {
+				return err
+			}
+			if l != 2 {
+				return fmt.Errorf("Bad array length %v", l)
+			}
+			t, err := decoder.PeekCode()
+			if err != nil {
+				return err
+			}
+			var ts time.Time
+			switch {
+			case t == msgpcode.Uint32:
+				tRaw, err := decoder.DecodeUint32()
+				if err != nil {
+					return err
+				}
+				ts = time.Unix(int64(tRaw), 0)
+			case msgpcode.IsExt(t):
+				fmt.Println("Ext")
+
+			case msgpcode.IsFixedExt(t):
+				fmt.Println("FixedExt")
+			default:
+				return fmt.Errorf("Unknown type %v", t)
+			}
+			record, err := decoder.DecodeMap()
+			if err != nil {
+				return err
+			}
+			events[i] = Event{tag, ts, record}
+		}
+		var option map[string]interface{}
+		if l == 3 {
+			option, err = decoder.DecodeMap()
+			if err != nil {
+				return err
+			}
+			fmt.Println("Option", option)
+		}
+		for _, event := range events {
+			err = f.eventHandler(event.tag, event.ts, event.record)
+			if err != nil {
+				return err
+			}
+		}
+
+	case msgpcode.IsBin(firstCode):
+		fmt.Println("Bin")
+
+	case firstCode == msgpcode.Uint32:
+		if l > 4 {
+			return fmt.Errorf("Message too large: %d", l)
+		}
+		ts, err := decoder.DecodeUint32()
+		if err != nil {
+			return err
+		}
+		record, err := decoder.DecodeMap()
+		if err != nil {
+			return err
+		}
+		if l == 4 {
+			option, err := decoder.DecodeMap()
+			if err != nil {
+				return err
+			}
+			fmt.Println("option", option)
+		}
+		return f.eventHandler(tag, time.Unix(int64(ts), 0), record)
+	default:
+		return fmt.Errorf("Bad code %v", firstCode)
+	}
+	return nil
+}
+
+func (f *FluentReader) doHelo() error { return nil }
+func (f *FluentReader) doPing() error { return nil }
+func (f *FluentReader) doPong() error { return nil }
--- server/server.go
+++ server/server.go
@@ -1,24 +1,21 @@
 package server
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"net"
-	"time"
 
-	"github.com/vmihailenco/msgpack/v5"
-	"github.com/vmihailenco/msgpack/v5/msgpcode"
+	"github.com/factorysh/fluent-server/message"
 )
 
-func New(eventHandler func(tag string, time time.Time, record map[string]interface{}) error) *Server {
+func New(handler message.HandlerFunc) *Server {
 	return &Server{
-		eventHandler: eventHandler,
+		reader: message.New(handler),
 	}
 }
 
 type Server struct {
-	eventHandler func(tag string, time time.Time, record map[string]interface{}) error
+	reader *message.FluentReader
 }
 
 func (s *Server) ListenAndServe(address string) error {
@@ -55,169 +52,13 @@ func (s *Server) ListenAndServe(address string) error {
 			return err
 		}
 		fmt.Println("Hello", conn.RemoteAddr())
-		go s.handler(conn)
-	}
-	return nil
-}
-
-func (s *Server) oneMessage(decoder *msgpack.Decoder) error {
-	code, err := decoder.PeekCode()
-	if err != nil {
-		return err
-	}
-	if code == msgpcode.Nil {
-		fmt.Println("Hearthbeat")
-		return nil
-	}
-	if !msgpcode.IsFixedArray(code) {
-		return errors.New("Not an array")
-	}
-	l, err := decoder.DecodeArrayLen()
-	if err != nil {
-		return err
-	}
-	if l == 0 {
-		return errors.New("Empty array")
-	}
-	if l > 10 {
-		return errors.New("Flood")
-	}
-	type_, err := decoder.DecodeString()
-	if err != nil {
-		return err
-	}
-	switch type_ {
-	case "HELO":
-		s.doHelo()
-	case "PING":
-		s.doPing()
-	case "PONG":
-		s.doPong()
-	default:
-		return s.doMessage(type_, decoder, l)
-	}
-	return nil
-
-}
-
-type Event struct {
-	tag    string
-	ts     time.Time
-	record map[string]interface{}
-}
-
-func (s *Server) doMessage(tag string, decoder *msgpack.Decoder, l int) error {
-	if l < 2 {
-		return errors.New("Too short")
-	}
-	firstCode, err := decoder.PeekCode()
-	if err != nil {
-		return err
-	}
-	switch {
-	case msgpcode.IsFixedArray(firstCode):
-		size, err := decoder.DecodeArrayLen()
-		if err != nil {
-			return err
-		}
-		events := make([]Event, size)
-		for i := 0; i < size; i++ {
-			c, err := decoder.PeekCode()
+		go func() {
+			defer conn.Close()
+			err := s.reader.Read(conn)
 			if err != nil {
-				return err
+				log.Println(err)
+				return
 			}
-			if !msgpcode.IsFixedArray(c) {
-				return fmt.Errorf("Elem %d is not an array : %v", i, c)
-			}
-			l, err := decoder.DecodeArrayLen()
-			if err != nil {
-				return err
-			}
-			if l != 2 {
-				return fmt.Errorf("Bad array length %v", l)
-			}
-			t, err := decoder.PeekCode()
-			if err != nil {
-				return err
-			}
-			var ts time.Time
-			switch {
-			case t == msgpcode.Uint32:
-				tRaw, err := decoder.DecodeUint32()
-				if err != nil {
-					return err
-				}
-				ts = time.Unix(int64(tRaw), 0)
-			case msgpcode.IsExt(t):
-				fmt.Println("Ext")
-
-			case msgpcode.IsFixedExt(t):
-				fmt.Println("FixedExt")
-			default:
-				return fmt.Errorf("Unknown type %v", t)
-			}
-			record, err := decoder.DecodeMap()
-			if err != nil {
-				return err
-			}
-			events[i] = Event{tag, ts, record}
-		}
-		var option map[string]interface{}
-		if l == 3 {
-			option, err = decoder.DecodeMap()
-			if err != nil {
-				return err
-			}
-			fmt.Println("Option", option)
-		}
-		for _, event := range events {
-			err = s.doEvent(event.tag, event.ts, event.record)
-		}
-
-	case msgpcode.IsBin(firstCode):
-		fmt.Println("Bin")
-
-	case firstCode == msgpcode.Uint32:
-		if l > 4 {
-			return fmt.Errorf("Message too large: %d", l)
-		}
-		ts, err := decoder.DecodeUint32()
-		if err != nil {
-			return err
-		}
-		record, err := decoder.DecodeMap()
-		if err != nil {
-			return err
-		}
-		if l == 4 {
-			option, err := decoder.DecodeMap()
-			if err != nil {
-				return err
-			}
-			fmt.Println("option", option)
-		}
-		return s.doEvent(tag, time.Unix(int64(ts), 0), record)
-	default:
-		return fmt.Errorf("Bad code %v", firstCode)
+		}()
 	}
-	return nil
-}
-
-func (s *Server) handler(conn net.Conn) {
-	defer conn.Close()
-	decoder := msgpack.NewDecoder(conn)
-	for {
-		err := s.oneMessage(decoder)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
-func (s *Server) doHelo() {}
-func (s *Server) doPing() {}
-func (s *Server) doPong() {}
-func (s *Server) doEvent(tag string, ts time.Time, record map[string]interface{}) error {
-	return s.eventHandler(tag, ts, record)
 }
