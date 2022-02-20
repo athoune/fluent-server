5aaff911bf3ee4ac8df2989f8604103c787cd214
Mathieu Lecarme <mlecarme@bearstech.com>
2022-02-20 19:46:29 +0100

    TLS draft.

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net"
@@ -15,34 +16,45 @@ func New(handler message.HandlerFunc) *Server {
 }
 
 type Server struct {
-	reader *message.FluentReader
+	reader    *message.FluentReader
+	useUDP    bool
+	useMTLS   bool
+	tlsConfig *tls.Config
 }
 
 func (s *Server) ListenAndServe(address string) error {
-	go func() {
-		a, err := net.ResolveUDPAddr("udp", address)
-		if err != nil {
-			panic(err)
-		}
-		listener, err := net.ListenUDP("udp", a)
-		if err != nil {
-			panic(err)
-		}
-		defer listener.Close()
-		buf := make([]byte, 1024)
-		for {
-			_, addr, err := listener.ReadFromUDP(buf)
+	if s.useUDP {
+		go func() {
+			a, err := net.ResolveUDPAddr("udp", address)
 			if err != nil {
-				fmt.Println(err)
-				return
+				panic(err)
 			}
-			re, _ := net.DialUDP("udp", nil, addr)
-			defer re.Close()
-			re.Write(buf)
-			fmt.Println("Pong")
-		}
-	}()
-	listener, err := net.Listen("tcp", address)
+			listener, err := net.ListenUDP("udp", a)
+			if err != nil {
+				panic(err)
+			}
+			defer listener.Close()
+			buf := make([]byte, 1024)
+			for {
+				_, addr, err := listener.ReadFromUDP(buf)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				re, _ := net.DialUDP("udp", nil, addr)
+				defer re.Close()
+				re.Write(buf)
+				fmt.Println("Pong")
+			}
+		}()
+	}
+	var listener net.Listener
+	var err error
+	if s.useMTLS {
+		listener, err = tls.Listen("tcp", address, s.tlsConfig)
+	} else {
+		listener, err = net.Listen("tcp", address)
+	}
 	if err != nil {
 		return err
 	}
