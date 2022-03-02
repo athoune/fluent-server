7074ac3aba32311d20828eab593f31fa9e2052b4
Mathieu Lecarme <mlecarme@bearstech.com>
2022-03-02 18:02:53 +0100

    Mirror test.

--- contrib/js/index.js
+++ contrib/js/index.js
@@ -1,4 +1,5 @@
 const FluentClient = require("@fluent-org/logger").FluentClient;
+const http = require("http");
 
 const logger = new FluentClient("tag_prefix", {
   socket: {
@@ -12,7 +13,31 @@ const logger = new FluentClient("tag_prefix", {
 
 
 async function demo() {
-    await logger.emit("my_tag", {name: "Bob", age: 42});
+    const magic = Math.random();
+    await logger.emit("my_tag", {
+      name: "Bob",
+      age: 42,
+      magic: magic,
+    });
+    http.get("http://127.0.0.1:24280", (res) => {
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            const parsedData = JSON.parse(rawData);
+            //console.log(parsedData);
+            let found = false;
+            for (evt of parsedData["tag_prefix.my_tag"]) {
+                if (evt.record.magic == magic) {
+                    found = true;
+                }
+            }
+            if (! found) {
+                throw "Event not found in the mirror server";
+            }
+            console.log("Test passed");
+        });
+    });
 };
 
 demo().then(() => {
